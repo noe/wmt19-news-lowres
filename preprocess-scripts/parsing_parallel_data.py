a40cc16b6d5d35bf9971630bfda09b773db79756
christine.raouf88@gmail.com <christine.raouf88@gmail.com>
2019-03-05 17:53:59 +0100

    parse file for parallel data

--- preprocess-scripts/parsing_parallel_data.py
+++ preprocess-scripts/parsing_parallel_data.py
@@ -1,24 +1,5 @@
 import re
 
-#split line by line and replace the tokens in the lines
-def read_file_line_by_line_with_special_handling(file, file_es, file_en_coref, file_en):
-    file_es = open(file_es, 'w')
-    file_en = open(file_en, 'w')
-    file_en_coref  = open(file_en_coref, 'w')
-    with open(file) as f:
-        for line in f:
-
-            lan=line.split('\t')
-            print (lan [0])
-            file_es.write(lan[0]+'\n')
-            print(lan[1])
-            file_en_coref.write(lan[1]+'\n')
-            #replace a regex of the coref
-            print(re.sub("<b_crf>.*?<e_crf> ", "", lan[1]))
-            en_line = re.sub("<b_crf>.*?<e_crf> ", "", lan[1])
-            file_en.write(en_line+'\n')
-
-            print ('**')
 
 
 #split line by line...but before using ..check its suitability to ur file
