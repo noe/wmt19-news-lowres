e9913c486c98e9b2687cf3013c288736a203a6fb
Noe Casas <noe.casas@gmail.com>
2019-03-07 15:30:56 +0100

    Make remaining global var local

--- data_scripts/generic/common.sh
+++ data_scripts/generic/common.sh
@@ -14,7 +14,7 @@ log() { echo "$*" 1>&2; }
 ### Function to get a random number sequence ##################################
 get_seeded_random(){
   # See https://www.gnu.org/software/coreutils/manual/html_node/Random-sources.html
-  seed="$1"
+  local seed="$1"
   openssl enc -aes-256-ctr -pass pass:"$seed" -nosalt </dev/zero 2>/dev/null
 }
 
