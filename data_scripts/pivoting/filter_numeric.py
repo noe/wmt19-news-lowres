dc15a830bd14157a653ba753eff799e344e60dd8
Noe Casas <noe.casas@gmail.com>
2019-03-13 18:23:01 +0100

    More stuff

--- data_scripts/pivoting/filter_numeric.py
+++ data_scripts/pivoting/filter_numeric.py
@@ -1,23 +1,28 @@
 #!/usr/bin/env python3
 
 import argparse
+import string
 import sys
 
 
-def is_sentence_ok(sentence, max_numeric_ratio):
-    ratio = sum(1. if c.isnumeric() else 0. for c in sentence) / len(sentence)
-    return ratio < max_numeric_ratio
+NUMBER_OR_PUNCTUATION = string.punctuation + " 0123456789"
+
+
+def is_sentence_ok(sentence, max_ratio):
+    length = len(sentence)
+    ratio = sum(1. if c in NUMBER_OR_PUNCTUATION else 0. for c in sentence) / length
+    return ratio < max_ratio
 
 
 def filter_lines(max_numeric_ratio, is_tsv):
     separator = "\t" if is_tsv else ""
     for line in sys.stdin:
-        line = line.strip()
+        line = line.strip('\r\n ')
         sentences = line.split(separator)[:2] if is_tsv else [line]
         is_ok = all(is_sentence_ok(s, max_numeric_ratio) for s in sentences)
         if not is_ok:
             continue
-        print(sentences[0] + separator + (sentences[1] if is_tsv else ""))
+        print("{}{}{}".format(sentences[0],separator, sentences[1] if is_tsv else ""))
 
 
 def main():
--- data_scripts/pivoting/prepare_pivot_data.sh
+++ data_scripts/pivoting/prepare_pivot_data.sh
@@ -16,6 +16,12 @@ SEED=3211
 clean_crawled_tsv(){
   grep -v '____' \
    | grep -v '\-\-\-\-' \
+   | grep -v '????' \
+   | grep -v '\*\*\*\*' \
+   | grep -v '++++++' \
+   | grep -v '_ _ _ _' \
+   | grep -v '\- \- \- \-' \
+   | grep -v '= = = =' \
    | $SCRIPT_DIR/filter_numeric.py --tsv --max-ratio 0.2
 }
 
@@ -43,36 +49,75 @@ prepare_enru_data(){
 
   cd $ENRU_DOWNLOAD_DIR
 
-  clean_corpus_aggressive en-ru/UNv1.0.en-ru en ru clean
-  clean_corpus_aggressive commoncrawl.ru-en en ru clean
-  clean_corpus_aggressive paracrawl-release1.en-ru.zipporah0-dedup-clean en ru clean
-  clean_corpus_aggressive corpus.en_ru.1m en ru clean
+  # ORIGINAL CORPUS SIZE -------------------
+  #
+  #  23M united nations
+  # 900K commoncrawl
+  #  12M paracrawl
+  #   1M yandex
+  # ----------------------------------------
 
-  EN_FILES="en-ru/UNv1.0.en-ru.clean.en commoncrawl.ru-en.clean.en paracrawl-release1.en-ru.zipporah0-dedup-clean.clean.en corpus.en_ru.1m.clean.en"
-  RU_FILES="en-ru/UNv1.0.en-ru.clean.ru commoncrawl.ru-en.clean.ru paracrawl-release1.en-ru.zipporah0-dedup-clean.clean.ru corpus.en_ru.1m.clean.ru"
 
-  TMP_FILE=$(mktemp)
-  paste <(cat $EN_FILES) <(cat $RU_FILES) \
+  declare -A corpus_sample_size=( ["en-ru/UNv1.0.en-ru"]=2000000 \
+                                  ["commoncrawl.ru-en"]=200000 \
+                                  ["paracrawl-release1.en-ru.zipporah0-dedup-clean"]=4000000 \
+                                  ["corpus.en_ru.1m"]=1000000)
+  # see hash tables in bash 4 at https://stackoverflow.com/a/3467959/674487
+
+  TMP_CORPUS=$(mktemp)
+  touch $TMP_CORPUS
+  for corpus_prefix in "${!corpus_sample_size[@]}"; do
+    clean_corpus_aggressive $corpus_prefix en ru clean
+    paste $corpus_prefix.clean.en $corpus_prefix.clean.ru \
+       | grep -v 'The time now is' \
+       | sort -u \
        | clean_crawled_tsv \
-       | shuf --random-source=<(get_seeded_random 111) \
-       > $TMP_FILE
+       | shuf --random-source=<(get_seeded_random 777) \
+       | head -"${corpus_sample_size[$corpus_prefix]}" >> $TMP_CORPUS
+    rm $corpus_prefix.clean.en $corpus_prefix.clean.ru
+  done  
 
-  cut -f 1 $TMP_FILE > $ENRU_OUTPUT_DIR/corpus.en-ru.en
-  cut -f 2 $TMP_FILE > $ENRU_OUTPUT_DIR/corpus.en-ru.ru
+  
+  cat $TMP_CORPUS | shuf --random-source=<(get_seeded_random 111) -o $TMP_CORPUS
 
-  rm $EN_FILES $RU_FILES $TMP_FILE
+  cut -f 1 $TMP_CORPUS > $ENRU_OUTPUT_DIR/corpus.en-ru.en
+  cut -f 2 $TMP_CORPUS > $ENRU_OUTPUT_DIR/corpus.en-ru.ru
+
+  rm $TMP_CORPUS
 }
 
 
 # Prepare Kk-Ru data ##########################################################
 
 prepare_kkru_data(){
-  KKRU_DOWNLOAD_DIR=$DOWNLOAD_DIR/kk-ru
-  # TODO
-  cut -f 1,2 crawl.kk-ru.tsv | sort -u
-  news-commentary-v14.kk-ru.tsv.gz
+  KKRU_DOWNLOAD_DIR=$(realpath $DOWNLOAD_DIR/kk-ru)
+  KKRU_OUTPUT_DIR=$(realpath $OUTPUT_DIR/kk-ru)
+
+  mkdir -p $KKRU_OUTPUT_DIR
+
+  cd $KKRU_DOWNLOAD_DIR
+  
+  TMP_CRAWL_DIR=$(mktemp -d)
+  mkdir -p %TMP_CRAWL_DIR
+
+  $TMP_CRAWL_PREFIX=$TMP_CRAWL_DIR/crawl.kk-ru
+
+  cut -f 1,2 crawl.kk-ru.tsv | sort -u | clean_crawled_tsv > $TMP_CRAWL_PREFIX.tsv
+  cut -f 1 $TMP_CRAWL_PREFIX.tsv > $TMP_CRAWL_PREFIX.kk
+  cut -f 2 $TMP_CRAWL_PREFIX.tsv > $TMP_CRAWL_PREFIX.ru
+
+  clean_corpus_aggressive $TMP_CRAWL_PREFIX kk ru clean
+
+  paste <(cat $TMP_CRAWL_PREFIX.kk) <(cat $TMP_CRAWL_PREFIX.ru) > $TMP_CRAWL_PREFIX.tsv
+  cat $TMP_CRAWL_PREFIX.tsv news-commentary-v14.kk-ru.tsv \
+       | sort -u \
+       | shuf --random-source=<(get_seeded_random 333) \
+       > $KKRU_OUTPUT_DIR
+
+  rm -rf $TMP_CRAWL_DIR
 }
 
 
-prepare_enru_data
+#prepare_enru_data
+prepare_kkru_data
 
