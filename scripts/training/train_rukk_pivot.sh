b757f41a4f5732875da694e657da7f5c33c3a149
Noe Casas <noe.casas@gmail.com>
2019-03-15 12:08:50 +0100

    Fix langs and data file names

--- scripts/training/train_rukk_pivot.sh
+++ scripts/training/train_rukk_pivot.sh
@@ -2,15 +2,9 @@
 
 
 : ${1:?"First argument is the model directory"}
-: ${2:?"Second argument is the training data prefix"}
-: ${3:?"Second argument is the dev data prefix"}
-: ${4:?"Third argument is the source language"}
-: ${5:?"Fourth argument is the target language"}
+: ${2:?"Second argument is the data root"}
 MODEL_DIR=$1
-TRAIN_DATA_PREFIX=$2
-DEV_DATA_PREFIX=$3
-SRC=$4
-TGT=$5
+DATA_ROOT=$2
 
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -18,5 +12,9 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # load common functions
 . $SCRIPT_DIR/../generic/common.sh
 
+TRAIN_DATA_PREFIX=$DATA_ROOT/pivoting/kk-ru/corpus.kk-ru.train.tok.tc
+DEV_DATA_PREFIX=$DATA_ROOT/pivoting/kk-ru/corpus.kk-ru.dev.tok.tc
+SRC=ru
+TGT=kk
 
 train_moses $MODEL_DIR $TRAIN_DATA_PREFIX $DEV_DATA_PREFIX $SRC $TGT 32000
