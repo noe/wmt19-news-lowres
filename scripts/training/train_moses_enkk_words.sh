613601d45edaf814038e9a806877de362f2f329e
Noe Casas <noe.casas@gmail.com>
2019-03-21 11:14:28 +0100

    Script to train word-level en->kk Moses

---
+++ scripts/training/train_moses_enkk_words.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -x
+
+
+: ${1:?"First argument is the model directory"}
+: ${2:?"Second argument is the data root"}
+MODEL_DIR=$1
+DATA_ROOT=$2
+
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# load common functions
+. $SCRIPT_DIR/../generic/common.sh
+
+TRAIN_DATA_PREFIX=$DATA_ROOT/baselines/train.tok.tc.clean
+DEV_DATA_PREFIX=$DATA_ROOT/baselines/dev.tok.tc
+SRC=en
+TGT=kk
+
+train_moses $MODEL_DIR $TRAIN_DATA_PREFIX $DEV_DATA_PREFIX $SRC $TGT 10000 word
