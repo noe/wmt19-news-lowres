de00bfb3fec906dca78dac39943f2c294f95e1b7
Noe Casas <noe.casas@gmail.com>
2019-04-14 00:41:35 +0200

    Add scripts to train moses on synthetic data

---
+++ scripts/training/train_final_kken_moses.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+: ${1:?"First argument is the model directory"}
+: ${2:?"Second argument is the data directory"}
+MODEL_DIR=$1
+DATA_DIR=$2
+
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# load common functions
+. ~/wmt19/repo/scripts/generic/common.sh
+
+TRAIN_DATA_PREFIX=$DATA_DIR/train_kken.tok.tc
+DEV_DATA_PREFIX=$DATA_DIR/dev.tok.tc
+SRC=kk
+TGT=en
+
+train_moses $MODEL_DIR $TRAIN_DATA_PREFIX $DEV_DATA_PREFIX $SRC $TGT x word
